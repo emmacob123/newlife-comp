91baf7e4f9750be27dcea33dac0fb77d0a18f918
emmacob123 <88585192+emmacob123@users.noreply.github.com>
2022-07-16 15:04:35 +0100

    update
    iu update

---
+++ istutorial-bootstrap/intro.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div>
+        <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+
+            <a class="me-3 py-2 text-dark text-decoration-none" href="#">home</a>
+            <a class="me-3 py-2 text-dark text-decoration-none" href="#">product</a>
+            <a class="me-3 py-2  text-dark text-decoration-none" href="#"> about us</a>
+        </nav>
+    </div>
+    <div>
+        <h1 style="color: blue ;">This is my class </h1>
+
+    </div>
+
+
+</body>
+
+</html>
